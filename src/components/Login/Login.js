8ce82cea63d0bb67ea71e5319fbabf4c5c53559c
sujon-mia <sujonmia.0612@gmail.com>
2022-04-10 19:34:22 +0600

    added onauthstate changed to persist the auth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 import './Login.css';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
             <h3>Please login</h3>
+            <div style={{ margin: '20px' }}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <form>
+                <input type="email" placeholder="Your Email" />
+                <br />
+                <input type="password" placeholder="Your Password" />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
